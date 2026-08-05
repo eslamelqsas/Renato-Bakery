d5057306de76cd73faa544217034d2a6bee6282c
eslamelqsas <eslamelqsas149@gmail.com>
2026-08-05 12:12:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿function orderItem(itemName) {
+
+    const phoneNumber = "201557036957";
+
+    const message =
+        "Hello Renato Bakery! I would like to order: " + itemName;
+
+    const whatsappURL =
+        "https://wa.me/" +
+        phoneNumber +
+        "?text=" +
+        encodeURIComponent(message);
+
+    window.open(whatsappURL, "_blank");
+}
